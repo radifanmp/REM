b1305741f0e48d832d0e25eda43ea8906f4e66a1
Radifan Mardhi Prana <radifanmp@gmail.com>
2019-01-19 19:59:46 +0700

    Layout

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,17 +1,7 @@
 @extends('layouts.main')
 
 @section('content')
-{{-- menu --}}
-<nav>
-    <div class="nav-wrapper">
-      <a style="padding-right: 50px" href="#" class=" right">Login</a>
-      <a style="padding-right: 50px" href="#" class=" right">Daftar</a>
-      <ul id="nav-mobile" class="left hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-    </div>
-  </nav>
 
+ 
+    </div>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('bs/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('bs/css/bootstrap-grid.min.css')}}">
+    {{-- <link rel="stylesheet" href="{{asset('bs/css/bootstrap.min.css')}}"> --}}
+    <link rel="stylesheet" href="{{asset('materialize/css/materialize.min.css')}}">
+    {{-- <link rel="stylesheet" href="{{asset('bs/css/bootstrap-grid.min.css')}}"> --}}
+    <script src="{{asset('materialize/js/materialize.min.js')}}"></script>
     <script src="{{asset('bs/js/jqry.min.js')}}"></script>
-    <script src="{{asset('bs/js/bootstrap.min.js')}}"></script>
+    {{-- <script src="{{asset('bs/js/bootstrap.min.js')}}"></script> --}}
 
     <title>@yield('title')</title>
 </head>
@@ -15,7 +17,49 @@
     <div id="wrapper">
         @yield('content')
     </div>
+   
+    {{-- menu --}}
+<nav>
+        <div class="nav-wrapper">
+          <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <li><a href="/index">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+          <ul id="nav-mobile2" class="right hide-on-med-and-down">
+          <li><a href="/login" >Login</a></li>
+          <li><a href="/daftar" >Daftar</a></li>
+          </ul>
+        </div>
+</nav>
     
+ {{-- jumbotron/section --}}
+ <div class="slider">
+        <ul class="slides">
+          <li>
+          <img src="{{asset('img/jbtrn/jbtrn1.jpg')}}"> <!-- random image -->
+            <div class="caption center-align">
+              <h3>This is our big Tagline!</h3>
+              <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+            </div>
+          </li>
+          <li>
+            <img src="{{asset('img/jbtrn/jbtrn2.jpg')}}"> <!-- random image -->
+            <div class="caption left-align">
+              <h3>Left Aligned Caption</h3>
+              <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+            </div>
+          </li>
+          <li>
+            <img src="{{asset('img/jbtrn/jbtrn3.jpg')}}"> <!-- random image -->
+            <div class="caption right-align">
+              <h3>Right Aligned Caption</h3>
+              <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+            </div>
+          </li>
+        </ul>
+
+    {{-- footer --}}
     <footer class="page-footer">
         <div class="footer-copyright">
           <div class="container">
@@ -24,5 +68,18 @@
           </div>
         </div>
       </footer>
+
+      {{-- slider jumbotron --}}
+      <script>
+          const slider = document.querySelector('.slider');
+          var responsive=Math.floor($(window ).height()*0.33);
+          console.log(responsive);
+          M.Slider.init(slider, {
+              indicators: false,
+              height: responsive,
+              transition: 500,
+              interval: 6000
+          });
+     </script>
 </body>
 </html>
