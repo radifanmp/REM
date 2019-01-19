51a2b98768e1c3c3594c846f01d7617db1485837
Radifan Mardhi Prana <radifanmp@gmail.com>
2019-01-19 18:52:42 +0700

    layout

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.main')
 
 @section('content')
-    INDEX
+{{-- menu --}}
+<nav>
+    <div class="nav-wrapper">
+      <a style="padding-right: 50px" href="#" class=" right">Login</a>
+      <a style="padding-right: 50px" href="#" class=" right">Daftar</a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('materialize/css/materialize.min.css')}}">
-    <script src="{{asset('materialize/js/materialize.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('bs/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('bs/css/bootstrap-grid.min.css')}}">
+    <script src="{{asset('bs/js/jqry.min.js')}}"></script>
+    <script src="{{asset('bs/js/bootstrap.min.js')}}"></script>
 
     <title>@yield('title')</title>
 </head>
@@ -13,5 +15,14 @@
     <div id="wrapper">
         @yield('content')
     </div>
+    
+    <footer class="page-footer">
+        <div class="footer-copyright">
+          <div class="container">
+          © 2014 Copyright Text
+          <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+          </div>
+        </div>
+      </footer>
 </body>
 </html>
