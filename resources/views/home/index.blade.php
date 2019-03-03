13244fa518fcdfb181d8f04ac7a232462c8f4338
Radifan Mardhi Prana <radifanmp@gmail.com>
2019-03-03 17:06:16 +0700

    gambar

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,6 +4,10 @@
 
 @section('content')
 
- 
+<div style = "width:404px; height:238px;" class="card">
+    <div class="card-image waves-effect">
+      <img class="activator" src="{{asset('img/masjidtest.jpg')}}">
+    </div>
+
     </div>
 @endsection
