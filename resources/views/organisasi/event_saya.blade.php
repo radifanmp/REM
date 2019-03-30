c5fa135db77625fa092e699c67a5771b8fb4a5e0
Faizal Ardian Putra <faizal@docotel.com>
2019-03-30 19:28:18 +0700

    notfound organisasi

--- resources/views/organisasi/event_saya.blade.php
+++ resources/views/organisasi/event_saya.blade.php
@@ -13,32 +13,39 @@
                 <button>Cari</button>
             </div>
         </div>
-        
-        @foreach ($data as $org)
-        
-        <div class="row">
-            <div class="col s6">
+        @if ($data)
+            @foreach ($data as $org)
+            
+            <div class="row">
+                <div class="col s6">
 
-                <div class="card">
-                    <div class="card-content">
-                        <div class="row">
-                            <div class="col s3">
-                                <img src="{{ $org->gambar_organisasi}}" class="circle responsive-img" style="width:100px;height:100px;">
-                            </div>
-                            <div class="col s9">
-                                <strong>{{ $org->nama_organisasi }}</strong><br>
-                                <span>{{ $org->alamat_organisasi }}</span><br>
-                                <span>Berdiri Sejak, {{ $dateHelper->convertDateIndo($org->created_date,['bulan','tahun']) }}</span><br>
-                                <span>{{ $org->nama_kabupaten }}, {{ $org->nama_provinsi }}</span>
+                    <div class="card">
+                        <div class="card-content">
+                            <div class="row">
+                                <div class="col s3">
+                                    <img src="{{ $org->gambar_organisasi}}" class="circle responsive-img" style="width:100px;height:100px;">
+                                </div>
+                                <div class="col s9">
+                                    <strong>{{ $org->nama_organisasi }}</strong><br>
+                                    <span>{{ $org->alamat_organisasi }}</span><br>
+                                    <span>Berdiri Sejak, {{ $dateHelper->convertDateIndo($org->created_date,['bulan','tahun']) }}</span><br>
+                                    <span>{{ $org->nama_kabupaten }}, {{ $org->nama_provinsi }}</span>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
+                </div>
+            </div>
+
+            @endforeach
+        @else
+        <div class="row">
+            <div class="col s12 center-align">
+                <img src="{{ $notfound }}" alt="Not Found">
             </div>
         </div>
-
-        @endforeach
+        @endif
 
     </div>
 
